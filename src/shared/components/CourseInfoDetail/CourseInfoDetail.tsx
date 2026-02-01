d5d3f228bf658448aaf69fe2fa477c248a473099
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2026-02-01 21:29:53 +0600

    feat: implement feature to cut extra text and place ... if authors' names goes to the next line

--- src/shared/components/CourseInfoDetail/CourseInfoDetail.tsx
+++ src/shared/components/CourseInfoDetail/CourseInfoDetail.tsx
@@ -1,3 +1,5 @@
+import styles from './CourseInfoDetail.module.css';
+
 function CourseInfoDetail({
   title,
   value,
@@ -6,9 +8,10 @@ function CourseInfoDetail({
   value: number | string;
 }) {
   return (
-    <div style={{ display: 'flex' }}>
-      <h4 style={{ fontWeight: '700px', fontSize: '16px' }}>{title}</h4>
-      <span style={{ fontSize: '16px' }}>{value}</span>
+    <div className={styles.infoRow}>
+      <h4 className={styles.infoTitle}>{title}</h4>
+      {'\u00A0'}
+      <span className={styles.infoValue}>{value}</span>
     </div>
   );
 }
